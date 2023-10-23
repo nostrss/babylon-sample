31581ac583a30ec20257c3ef921333b4f637a31d
nostrss <jintagi@gmail.com>
2023-10-23 20:33:37 +0900

    add emotion

---
+++ src/styles/globals.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+export const globalStyles = () => css`
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+`;
+
+export default globalStyles;
